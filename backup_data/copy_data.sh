c2b973702e78a50db6a964bdbf21907dca4262f5
remoroethlisberger <remo.r@gmx.ch>
2017-05-17 21:10:34 +0200

    Changed PATH to TARGET, unfortunate naming, overwriting the actual PATH parameter

--- backup_data/copy_data.sh
+++ backup_data/copy_data.sh
@@ -1,5 +1,5 @@
 # Keeps daily copies of files and folders in a SOURCE folder copied to the PATH specified
-readonly PATH="/mypath/folder/"$(date +%Y%m%d)
+readonly TARGET="/mypath/folder/"$(date +%Y%m%d)
 readonly SOURCE="/mypath/folder/source"
-mkdir "$PATH"
+mkdir "$TARGET"
 cp -r "$SOURCE" "$PATH"
